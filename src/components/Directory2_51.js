19c13c9e57c6a661ff2624f25a493ff19676706a
nnj-wp <nnj448jack@gmail.com>
2021-10-24 21:24:32 +0800

    move crown_theme to src

--- src/components/Directory2_51.js
+++ src/components/Directory2_51.js
@@ -1,29 +1,32 @@
-// import React, { useState } from 'react';
-// import './Directory_51.scss';
-// // import items from './menu-items-data';
-// import axios from 'axios'
-// import MenuItem_51 from './MenuItem_51';
+import React, { useState, useEffect } from 'react';
+import './Directory_51.scss';
+// import items from './menu-items-data';
+import axios from 'axios';
+import MenuItem_51 from './MenuItem_51';
 
-// const Directory2_51 = () => {
-//   const [menuItems, setMenuItems] = useState(items);
-//   console.log('menuItems', menuItems);
-//   return (
-//     <div>
-//       <div className="directory-menu">
-//         {menuItems.map((item) => {
-//           const { id, name, remoteUrl, size } = item;
-//           return (
-//             <MenuItem_51
-//               key={id}
-//               name={name}
-//               remoteUrl={remoteUrl}
-//               size={size}
-//             />
-//           );
-//         })}
-//       </div>
-//     </div>
-//   );
-// };
+const Directory2_51 = () => {
+  const [menuItems, setMenuItems] = useState([]);
 
-// export default Directory2_51;
+  useEffect(() => {}, [])
+
+  console.log('menuItems', menuItems);
+  return (
+    <div>
+      <div className="directory-menu">
+        {menuItems.map((item) => {
+          const { id, name, remoteUrl, size } = item;
+          return (
+            <MenuItem_51
+              key={id}
+              name={name}
+              remoteUrl={remoteUrl}
+              size={size}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Directory2_51;
